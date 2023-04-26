47707ffc7378a3e62938e661b91b6a1cccee6a90
dundun <dundun6x@gmail.com>
2023-04-26 08:55:58 +0800

    just initial commit

---
+++ Assets/Scripts/SimpleLinearMotion.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleLinearMotion : Motion
+{
+    public float moveSpeed;
+    // public Vector2 
+    
+    public override void Start()
+    {
+        
+    }
+
+    public override void Act()
+    {
+        
+    }
+
+    public override void End()
+    {
+        
+    }
+}
